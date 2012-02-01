87bddd935ba99c57a05e43f8fd94e8889d51582b
fabian <fabian.closson@gmail.com>
2012-02-01 16:20:25 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roomedy::Application.config.secret_token = '593d180af0be1fc2b597d54a75357ef75f7f5a9e94eb0010add89d677e4564e3a602be3fce361e5c0e2dd17abb4f92758aecb5b307478d9c7f932b6ca689adb1'
